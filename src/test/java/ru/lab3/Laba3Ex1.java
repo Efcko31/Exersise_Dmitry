af536211afcca0a28328ca35ebed7983135c54c6
Илья <sheremetv1998@gmail.com>
2023-12-08 08:41:30 +0300

    Laba3. Ex. 1 - Redone it.

--- src/test/java/ru/lab3/Laba3Ex1.java
+++ src/test/java/ru/lab3/Laba3Ex1.java
@@ -11,27 +11,22 @@ public class Laba3Ex1 {
     void testEx1(){
 
         Assertions.assertEquals("2 2 ", testEx1(4));
-        Assertions.assertEquals("2 3 5 5 31",testEx1(4650));
+        Assertions.assertEquals("2 3 5 5 31 ",testEx1(4650));
         Assertions.assertEquals("", testEx1(0));
         Assertions.assertEquals("",testEx1(1));
+        Assertions.assertEquals("61 ",testEx1(61));
 
     }
 
     private String testEx1(int nubmer) {
         String answer = new String("");
+        int totalNum = 2;
         while(nubmer > 1) {
-            if (nubmer % 2 == 0) {
-                answer = answer.concat("2 ");
-                nubmer = nubmer / 2;
-            } else if (nubmer % 3 == 0) {
-                answer = answer.concat("3 ");
-                nubmer = nubmer / 3;
-            } else if (nubmer % 5 == 0) {
-                answer = answer.concat("5 ");
-                nubmer = nubmer / 5;
-            } else {
-                answer = answer.concat(Integer.toString(nubmer));
-                nubmer = 1;
+            if (nubmer % totalNum == 0){
+                answer = answer.concat(Integer.toString(totalNum));
+                answer = answer.concat(" ");
+                nubmer = nubmer / totalNum;
+            } else { totalNum++;
             }
         }
         return answer.toString();
